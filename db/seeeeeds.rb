9b2dbbe675f79a47e61f444570b1e637b084300f
pmills88 <pmillerk88@gmail.com>
2014-10-29 17:08:01 -0400

    Finished Carousel Prototype

---
+++ db/seeeeeds.rb
@@ -0,0 +1,15 @@
+	{
+		charity_name: "The Nature Conservancy",
+		charity_logo: "http://s30.postimg.org/v3ag9ebk1/TNC.png",
+		charity_desc: 'An American charitable environmental organization, headquartered in Arlington, Virginia. Its mission is to "conserve the lands and waters on which all life depends."',
+	},
+	{
+		charity_name: "United States Fund for UNICEF",
+		charity_logo: "http://s30.postimg.org/vsyaivif5/UNICEF.png",
+		charity_desc: "Does whatever it takes to save and protect the world's most vulnerable children.",
+	},
+	{
+		charity_name: "World Vision",
+		charity_logo: "http://s30.postimg.org/gvpthv56p/worldvision.png",
+		charity_desc: "Sponsor a child and help fight world hunger through your donations that support World Vision's aid, child nutrition and agricultural solutions around the world.",
+	},
