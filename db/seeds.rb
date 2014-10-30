e26478d0e37023faadaa102d193cc8e14b924843
pmills88 <pmillerk88@gmail.com>
2014-10-30 16:25:30 -0400

    Working Presentable Version, still so much to do :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,21 @@ charity_data = CharityData.create([
 		charity_desc: "Amazing people who do amazing things."
 	},
 	{
+		charity_name: "The Nature Conservancy",
+		charity_logo: "http://s30.postimg.org/v3ag9ebk1/TNC.png",
+		charity_desc: 'An American charitable environmental organization, headquartered in Arlington, Virginia. Its mission is to "conserve the lands and waters on which all life depends."',
+	},
+	{
+		charity_name: "United States Fund for UNICEF",
+		charity_logo: "http://s30.postimg.org/vsyaivif5/UNICEF.png",
+		charity_desc: "Does whatever it takes to save and protect the world's most vulnerable children.",
+	},
+	{
+		charity_name: "World Vision",
+		charity_logo: "http://s30.postimg.org/gvpthv56p/worldvision.png",
+		charity_desc: "Sponsor a child and help fight world hunger through your donations that support World Vision's aid, child nutrition and agricultural solutions around the world.",
+	},
+	{
 		charity_name: "Susan G. Komen Foundation",
 		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
 		charity_desc: "Breast Cancer Research."
