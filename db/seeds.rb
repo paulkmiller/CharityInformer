64cb815a8bff023244da3afda41a58df83996b48
pmills88 <pmillerk88@gmail.com>
2014-10-31 09:59:29 -0400

    OMG

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,44 @@ charity_data = CharityData.create([
 		charity_desc: "Amazing people who do amazing things."
 	},
 	{
-		charity_name: "World Vision",
-		charity_logo: "http://s30.postimg.org/gvpthv56p/worldvision.png",
-		charity_desc: "Sponsor a child and help fight world hunger through your donations that support World Vision's aid, child nutrition and agricultural solutions around the world.",
+		charity_name: "Susan G. Komen Foundation",
+		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
+		charity_desc: "Breast Cancer Research."
+	},
+	{
+		charity_name: "American Red Cross",
+		charity_logo: "http://s8.postimg.org/l7z71fv5x/American_Red_Cross.png",
+		charity_desc: "Disaster relief at home and abroad, CPR certification and first aid courses, blood donation, and emergency preparedness.",
+	},
+	{
+		charity_name: "World Wildlife Fund",
+		charity_logo: "http://s30.postimg.org/5upmfq6m9/panda_wwf_logo.png",
+		charity_desc: "The leading organization in wildlife conservation and endangered species.",
+	},
+	{
+		charity_name: "CARE",
+		charity_logo: "http://s8.postimg.org/jhg5zydmt/logo_care.gif",
+		charity_desc: "A major international humanitarian agency delivering broad-spectrum emergency relief and long-term international development projects.",
+	},
+	{
+		charity_name: "Save the Children",
+		charity_logo: "http://s8.postimg.org/aqdqjfjph/save_the_children_logo.png",
+		charity_desc: "An international non-governmental organization that promotes children's rights, provides relief and helps support children in developing countries.",
+	},
+	{
+		charity_name: "Oxfam America",
+		charity_logo: "http://s8.postimg.org/c45deqiyt/oxfam.png",
+		charity_desc: "An international confederation of 17 organizations working in approximately 94 countries worldwide to find solutions to poverty and what it considers as injustice around the world.",
+	},
+	{
+		charity_name: "Natural Resources Defense Council",
+		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
+		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
+	},
+	{
+		charity_name: "Doctors Without Borders",
+		charity_logo: "http://s8.postimg.org/819r1wzgl/DWB.png",
+		charity_desc: "Amazing people who do amazing things."
 	},
 	{
 		charity_name: "Susan G. Komen Foundation",
@@ -46,6 +81,166 @@ charity_data = CharityData.create([
 		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
 		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
 	},
+	{
+		charity_name: "Doctors Without Borders",
+		charity_logo: "http://s8.postimg.org/819r1wzgl/DWB.png",
+		charity_desc: "Amazing people who do amazing things."
+	},
+	{
+		charity_name: "Susan G. Komen Foundation",
+		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
+		charity_desc: "Breast Cancer Research."
+	},
+	{
+		charity_name: "American Red Cross",
+		charity_logo: "http://s8.postimg.org/l7z71fv5x/American_Red_Cross.png",
+		charity_desc: "Disaster relief at home and abroad, CPR certification and first aid courses, blood donation, and emergency preparedness.",
+	},
+	{
+		charity_name: "World Wildlife Fund",
+		charity_logo: "http://s30.postimg.org/5upmfq6m9/panda_wwf_logo.png",
+		charity_desc: "The leading organization in wildlife conservation and endangered species.",
+	},
+	{
+		charity_name: "CARE",
+		charity_logo: "http://s8.postimg.org/jhg5zydmt/logo_care.gif",
+		charity_desc: "A major international humanitarian agency delivering broad-spectrum emergency relief and long-term international development projects.",
+	},
+	{
+		charity_name: "Save the Children",
+		charity_logo: "http://s8.postimg.org/aqdqjfjph/save_the_children_logo.png",
+		charity_desc: "An international non-governmental organization that promotes children's rights, provides relief and helps support children in developing countries.",
+	},
+	{
+		charity_name: "Oxfam America",
+		charity_logo: "http://s8.postimg.org/c45deqiyt/oxfam.png",
+		charity_desc: "An international confederation of 17 organizations working in approximately 94 countries worldwide to find solutions to poverty and what it considers as injustice around the world.",
+	},
+	{
+		charity_name: "Natural Resources Defense Council",
+		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
+		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
+	},
+	{
+		charity_name: "Doctors Without Borders",
+		charity_logo: "http://s8.postimg.org/819r1wzgl/DWB.png",
+		charity_desc: "Amazing people who do amazing things."
+	},
+	{
+		charity_name: "Susan G. Komen Foundation",
+		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
+		charity_desc: "Breast Cancer Research."
+	},
+	{
+		charity_name: "American Red Cross",
+		charity_logo: "http://s8.postimg.org/l7z71fv5x/American_Red_Cross.png",
+		charity_desc: "Disaster relief at home and abroad, CPR certification and first aid courses, blood donation, and emergency preparedness.",
+	},
+	{
+		charity_name: "World Wildlife Fund",
+		charity_logo: "http://s30.postimg.org/5upmfq6m9/panda_wwf_logo.png",
+		charity_desc: "The leading organization in wildlife conservation and endangered species.",
+	},
+	{
+		charity_name: "CARE",
+		charity_logo: "http://s8.postimg.org/jhg5zydmt/logo_care.gif",
+		charity_desc: "A major international humanitarian agency delivering broad-spectrum emergency relief and long-term international development projects.",
+	},
+	{
+		charity_name: "Save the Children",
+		charity_logo: "http://s8.postimg.org/aqdqjfjph/save_the_children_logo.png",
+		charity_desc: "An international non-governmental organization that promotes children's rights, provides relief and helps support children in developing countries.",
+	},
+	{
+		charity_name: "Oxfam America",
+		charity_logo: "http://s8.postimg.org/c45deqiyt/oxfam.png",
+		charity_desc: "An international confederation of 17 organizations working in approximately 94 countries worldwide to find solutions to poverty and what it considers as injustice around the world.",
+	},
+	{
+		charity_name: "Natural Resources Defense Council",
+		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
+		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
+	},
+	{
+		charity_name: "Doctors Without Borders",
+		charity_logo: "http://s8.postimg.org/819r1wzgl/DWB.png",
+		charity_desc: "Amazing people who do amazing things."
+	},
+	{
+		charity_name: "Susan G. Komen Foundation",
+		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
+		charity_desc: "Breast Cancer Research."
+	},
+	{
+		charity_name: "American Red Cross",
+		charity_logo: "http://s8.postimg.org/l7z71fv5x/American_Red_Cross.png",
+		charity_desc: "Disaster relief at home and abroad, CPR certification and first aid courses, blood donation, and emergency preparedness.",
+	},
+	{
+		charity_name: "World Wildlife Fund",
+		charity_logo: "http://s30.postimg.org/5upmfq6m9/panda_wwf_logo.png",
+		charity_desc: "The leading organization in wildlife conservation and endangered species.",
+	},
+	{
+		charity_name: "CARE",
+		charity_logo: "http://s8.postimg.org/jhg5zydmt/logo_care.gif",
+		charity_desc: "A major international humanitarian agency delivering broad-spectrum emergency relief and long-term international development projects.",
+	},
+	{
+		charity_name: "Save the Children",
+		charity_logo: "http://s8.postimg.org/aqdqjfjph/save_the_children_logo.png",
+		charity_desc: "An international non-governmental organization that promotes children's rights, provides relief and helps support children in developing countries.",
+	},
+	{
+		charity_name: "Oxfam America",
+		charity_logo: "http://s8.postimg.org/c45deqiyt/oxfam.png",
+		charity_desc: "An international confederation of 17 organizations working in approximately 94 countries worldwide to find solutions to poverty and what it considers as injustice around the world.",
+	},
+	{
+		charity_name: "Natural Resources Defense Council",
+		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
+		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
+	},
+	{
+		charity_name: "Doctors Without Borders",
+		charity_logo: "http://s8.postimg.org/819r1wzgl/DWB.png",
+		charity_desc: "Amazing people who do amazing things."
+	},
+	{
+		charity_name: "Susan G. Komen Foundation",
+		charity_logo: "http://s8.postimg.org/8s2h7p1tx/komen_ribbon.png",
+		charity_desc: "Breast Cancer Research."
+	},
+	{
+		charity_name: "American Red Cross",
+		charity_logo: "http://s8.postimg.org/l7z71fv5x/American_Red_Cross.png",
+		charity_desc: "Disaster relief at home and abroad, CPR certification and first aid courses, blood donation, and emergency preparedness.",
+	},
+	{
+		charity_name: "World Wildlife Fund",
+		charity_logo: "http://s30.postimg.org/5upmfq6m9/panda_wwf_logo.png",
+		charity_desc: "The leading organization in wildlife conservation and endangered species.",
+	},
+	{
+		charity_name: "CARE",
+		charity_logo: "http://s8.postimg.org/jhg5zydmt/logo_care.gif",
+		charity_desc: "A major international humanitarian agency delivering broad-spectrum emergency relief and long-term international development projects.",
+	},
+	{
+		charity_name: "Save the Children",
+		charity_logo: "http://s8.postimg.org/aqdqjfjph/save_the_children_logo.png",
+		charity_desc: "An international non-governmental organization that promotes children's rights, provides relief and helps support children in developing countries.",
+	},
+	{
+		charity_name: "Oxfam America",
+		charity_logo: "http://s8.postimg.org/c45deqiyt/oxfam.png",
+		charity_desc: "An international confederation of 17 organizations working in approximately 94 countries worldwide to find solutions to poverty and what it considers as injustice around the world.",
+	},
+	{
+		charity_name: "Natural Resources Defense Council",
+		charity_logo: "http://s27.postimg.org/ibf8i8069/NRDC.png",
+		charity_desc: "Works to safeguard the earth — its people, its plants and animals, and the natural systems on which all life depends. ",
+	}
 	])
 
 charity_expenses = CharityExpense.create([
